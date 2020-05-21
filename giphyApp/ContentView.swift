ae27869feea8d136de4a4b9776cdcfcbb333648e
Denys <den4iklvivua@gmail.com>
2020-05-21 07:18:23 +0300

    Initial Commit

---
+++ giphyApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  giphyApp
+//
+//  Created by MacBook on 5/21/20.
+//  Copyright © 2020 den4iklvivua. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
